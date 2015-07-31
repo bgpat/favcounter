dfc8e7710e9e790c1e73f4f457ad0333eb28e44e
bgpat <bgpat@bgpat.net>
2015-07-31 14:21:19 +0900

    データ更新テストコードを更新

--- test.js
+++ test.js
@@ -8,7 +8,24 @@ Account.getAll((err, accounts) => {
     var tw = new Twitter(accounts[i]);
     var id = accounts.slice(i, i += 100).map(a => a.id);
     tw.get('users/lookup', {user_id: id.join(',')}, (err, data) => {
-      console.log(data);
+      var len = data.length;
+      data.forEach(d => {
+        Account(d.id_str, (err, a) => {
+          if (err) {
+            len--;
+            return console.error(err);
+          }
+          a.addData(d);
+          a.push(err => {
+            err && console.error(err);
+            if (--len === 0) {
+              process.exit();
+            } else {
+              console.log('remain', len);
+            }
+          });
+        });
+      });
     });
   }
 });
