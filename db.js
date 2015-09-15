36ef24ebfd953b31885e78ae70c973200d4bd786
bgpat <bgpat@bgpat.net>
2015-09-15 15:38:06 +0900

    エラー情報の出力先をエラー出力に変更

--- db.js
+++ db.js
@@ -5,5 +5,5 @@ var config = require('./config');
 
 var client = redis.createClient(config.redis.socket);
 client.on('error', e => e && console.error(e));
-client.select(config.redis.db, e => e && console.log(e));
+client.select(config.redis.db, e => e && console.error(e));
 module.exports = client;
